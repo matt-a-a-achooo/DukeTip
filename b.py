d747d9fa4463e69e077073abd0d49b872c9a4683
matthew <matthew.j.ku@gmail.com>
2017-06-20 15:48:20 -0400

    arrays

--- b.py
+++ b.py
@@ -1,15 +1,15 @@
-import numpy as np
+import numpy as np #imports numpy
 
-onesM = np.ones((4, 5))
-zeros = np.zeros((1, 5))
+onesM = np.ones((4, 5)) # creates a 5 by 4 array
+zeros = np.zeros((1, 5)) # creates a 5 by 1 array
 print(onesM)
 twos = zeros
-twos[0,0] = 1
+twos[0,0] = 1 # prints a 1 in the 0 index then the rest are o
 print(twos)
-print onesM * zeros
-onesM[1, 2] = 10
+print onesM * zeros # multiples 5by4 and the 5by1
+onesM[1, 2] = 10 # replaces 3rd number in the second row to 10
 print(onesM)
-print onesM * 2
-print(onesM.sum())
+print onesM * 2 #print the 5by4 times 2
+print(onesM.sum()) # adds everything
 print(onesM.sum(axis=0))
 print(onesM.sum(axis=1))
